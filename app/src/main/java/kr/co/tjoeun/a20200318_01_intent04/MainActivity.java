83e36ea4fb96a6ba2c0f1d7ec397e5e8910fad11
kws970609 <kws970609@users.noreply.github.com>
2020-03-18 10:13:45 +0900

    돌아온 화면에서 데이터 반영

--- app/src/main/java/kr/co/tjoeun/a20200318_01_intent04/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_01_intent04/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.a20200318_01_intent04;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
@@ -30,9 +31,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1000);
             }
         });
-    }
-//        2.프로필 수정화면에서 이름 입력 => 확인 => 종료 /메인으로 복귀
+    //        2.프로필 수정화면에서 이름 입력 => 확인 => 종료 /메인으로 복귀
 //        3. 돌아온 메인화면에서 2에서 입력한 이름을 텍스트뷰에 반영
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        requestCode => 어떤화면으로 띄운화면인지? 구별.
+//        이름변경 or 프사 변경 개발자가 분류한 requestCode값을 따라감.
+
+        if(requestCode == 1000) {
+//            우리가 요청한 프로필 변경 화면 이동이다!
+
+//            resultCode => 완료 /취소 여부. 보통 완료 일때만 대응.
+            if (resultCode == RESULT_OK) {
+//                확인 버튼을 눌러서 finish가 되었다!
+
+//                첨부된 데이터가 있나 확인. => Intent data 변수에 들어있다.
+
+                String name = data.getStringExtra("inputName");
+                binding.nameTxt.setText(name);
+
+            }
+
+        }
+
+
+
+
+
 
     }
 }
